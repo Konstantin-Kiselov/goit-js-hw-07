9775f20d4c2fc9552cbf589d4ae6f96ced57e19e
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-07-10 13:22:07 +0300

    submit task 6

---
+++ js/task-06.js
@@ -0,0 +1,20 @@
+const getInput = document.querySelector('#validation-input');
+// console.log(inputRef);
+
+const getDataLength = Number(getInput.getAttribute('data-length'));
+
+const inputIsValid = getInput.addEventListener('blur', onFormInput);
+
+function onFormInput(event) {
+
+  const getInputValue = event.currentTarget.value;
+
+  if (getInputValue.length === getDataLength) {
+    getInput.classList.add('valid');
+    getInput.classList.remove('invalid');
+  }
+  else {
+    getInput.classList.remove('valid');
+    getInput.classList.add('invalid');
+  }
+};
